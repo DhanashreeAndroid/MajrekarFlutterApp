ab33ae877c7d2727ef667ebcd079ba5e599d0bbf
pratik gupta <prati1907@gmail.com>
2024-05-13 14:45:19 +0530

    143 push on Test Flight Appstore

--- lib/CommonWidget/Constant.dart
+++ lib/CommonWidget/Constant.dart
@@ -1,7 +1,5 @@
-
-class Constant{
-  static const String baseUrl =
-      "http://api.majrekar-esoftware.com:9001/api/";
+class Constant {
+  static const String baseUrl = "http://api.majrekar-esoftware.com:9001/api/";
   static const String jwtToken = "Account/Token";
   static const String getData = "Vidhansabha/getdata";
   static const String getUserData = "Account/getuserdetail";
@@ -15,15 +13,12 @@ class Constant{
   //constant for app flow
 
   static const bool isOffline = true;
-  static const bool isShowShareImage = false;
-  static const bool isShowImageToHeader = false;
-  static const bool isDateLimit = true;
+  static const bool isShowShareImage = true;
+  static const bool isShowImageToHeader = true;
+  static const bool isDateLimit = false;
   static const String limitDate = "2024-05-25 00:00:00";
 
   static const String userName = "143";
   static const String password = "Rav33";
   static const String vidhansabhaName = "143 Dombivali";
-
-
 }
-
